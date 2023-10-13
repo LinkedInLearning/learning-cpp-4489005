8344fc08dccf2e487117bac8ee75ff6d863d9cb5
Eduardo Corpeño <kuashio@gmail.com>
2023-10-13 16:49:10 +0000

    updating code

--- src/Ch03/03_04/Begin/CodeDemo.cpp
+++ src/Ch03/03_04/Begin/CodeDemo.cpp
@@ -6,11 +6,9 @@
 #include <string>
 #include "cow.h"
 
-using namespace std;
-
 int main(){
     cow my_cow("Hildy",7,pet);
-    cout << my_cow.get_name() << " is a type-" << (int)my_cow.get_purpose() << " cow." << endl;
-    cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << endl;
+    cout << my_cow.get_name() << " is a type-" << (int)my_cow.get_purpose() << " cow." << std::endl;
+    cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
     return (0);
 }
