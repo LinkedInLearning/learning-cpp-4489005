5d374ab7a30357f13efd60575e772c2534827cb5
Eduardo Corpeño <kuashio@gmail.com>
2023-10-23 20:01:04 +0000

    adapted the pointers exercise

--- src/Ch03/03_06/End/CodeDemo.cpp
+++ src/Ch03/03_06/End/CodeDemo.cpp
@@ -4,12 +4,10 @@
 
 #include <iostream>
 #include <string>
-#include "cow.h"
 
 int main(){
     int a = 37;
     int *ptr;
-    cow *my_cow;
 
     ptr = &a;
 
@@ -18,13 +16,6 @@ int main(){
     std::cout << "           The address of a is " << &a << std::endl;
     std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
     std::cout << "         The address of ptr is " << &ptr << std::endl;
-    std::cout << std::endl;
-
-    my_cow = new cow("Gertie",3,cow_purpose::hide);
-    std::cout << my_cow->get_name() << " is a type-" << (int)my_cow->get_purpose() << " cow." << std::endl;
-    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
-    
-    delete my_cow;
 	
     std::cout << std::endl << std::endl;
     return (0);
