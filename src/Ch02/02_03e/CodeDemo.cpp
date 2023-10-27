353ab11a16f3b35bf473a2e5596633756159dd3d
Eduardo Corpeño <kuashio@gmail.com>
2023-10-27 19:56:19 +0000

    fixed spacing

--- src/Ch02/02_03e/CodeDemo.cpp
+++ src/Ch02/02_03e/CodeDemo.cpp
@@ -19,11 +19,11 @@ int main(){
     std::cout << "flag = " << my_flag << std::endl;
     my_flag = true;
     std::cout << "flag = " << my_flag << std::endl;
-    std::cout << "a+b = " << a+b << std::endl;
-    std::cout << "b-a = " << b-a << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
     unsigned int positive;
     positive = b - a;
-    std::cout << "b-a (unsigned) = " << positive << std::endl;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
